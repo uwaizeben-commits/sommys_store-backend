d846995fbe205b03114aa5f2b5dd9a9776141bb2
Developer <dev@sommys.store>
2026-02-19 21:12:54 -1100

    chore: remove tracked local files and update .gitignore

--- src/models/Product.js
+++ src/models/Product.js
@@ -4,6 +4,7 @@ const ProductSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     price: { type: Number, required: true, default: 0 },
+    quantity: { type: Number, required: true, default: 0 },
     category: { type: String },
     description: { type: String },
     images: [{ type: String }],
